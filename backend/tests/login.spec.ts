967c71a5d41ce4393ed21373a18420fa7da8b1c7
LorenzoBerg <lorenzobegnozzi@hotmail.com>
2025-02-25 19:04:21 -0300

    tests

--- backend/tests/login.spec.ts
+++ backend/tests/login.spec.ts
@@ -1,18 +1,42 @@
 import { test, expect, request } from '@playwright/test';
 
-test.describe('Login', () => {
-    let apiRequest = request.newContext({ baseURL: 'http://localhost:5000' });
 
-    test('Deve autenticar usuárioooo', async () => {
-        const res = await (await apiRequest).post('/api/auth/login', {
-            data: {
-                email: "teste.teste1@hotmail.com",
-                password: "123"
-            },
-            headers: { 'Content-Type': 'application/json' }
-        });
+test("login - emeil e senha", async ({ page }) => {
+    await page.goto('http://localhost:3000/login');
+    await page.getByPlaceholder('E-mail').fill('lorenzo.berg4@hotmail.com');
+    await page.getByPlaceholder('Senha').fill('123');
+    await page.waitForTimeout(1000)
+    await page.getByText('Entrar').click();
+    await expect(page.locator('.swal2-html-container')).toHaveText('Bem-vindo de volta!')
+})
 
-        expect(res.status()).toBe(200);
-        expect(await res.json()).toMatchObject({ token: expect.any(String), userId: expect.any(String) });
-    });
-});
+
+test("login - sem senha", async ({ page }) => {
+    await page.goto('http://localhost:3000/login');
+    await page.getByPlaceholder('E-mail').fill('lorenzo.berg4@hotmail.com');
+    await page.waitForTimeout(1000)
+    await page.getByText('Entrar').click();
+    await expect(page.locator('.swal2-html-container')).toHaveText('Senha incorreta')
+})
+
+test("login - sem E-mail", async ({ page }) => {
+    await page.goto('http://localhost:3000/login');
+    await page.getByPlaceholder('Senha').fill('123');
+    await page.waitForTimeout(1000)
+    await page.getByText('Entrar').click();
+
+    await expect(page.locator('.swal2-html-container')).toHaveText('Usuário não encontrado')
+})
+
+test("login - senha incorreta", async ({ page }) => {
+    await page.goto('http://localhost:3000/login');
+    await page.getByPlaceholder('E-mail').fill('lorenzo.berg4@hotmail.com');
+    await page.getByPlaceholder('Senha').fill('dlskkfj');
+    await page.waitForTimeout(1000)
+    await page.getByText('Entrar').click();
+
+    await expect(page.locator('.swal2-html-container')).toHaveText('Senha incorreta')
+})
+
+
+
